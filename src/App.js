3bf24e38c759df56be3ac734f4cb63f8d233ad54
Junayed5 <mdjunayed@gmail.com>
2023-01-27 16:10:17 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/main/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/main/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { BsMinecartLoaded, BsPerson, BsSearch } from 'react-icons/bs';
+import { CiHeart } from 'react-icons/ci'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex justify-between'>
+                <div>
+                    <img src="https://i.ibb.co/P1dqzww/final-png-1.png" alt="" />
+
+                </div>
+                <div className='flex my-5'>
+                    <form action="">
+                        <input className='border h-[50px] w-[713px] bg-[#F6F6F6] rounded-l-md' type="text" />
+                    </form>
+                    <div className='h-[50px] w-[65px] bg-[#0198E9] text-white rounded-r-md'>
+                        <BsSearch className='mx-auto my-4'/>
+                    </div>
+                </div>
+                <div className='flex gap-[42px] my-8'>
+                    <div className='flex'>
+                        <BsPerson className='mt-1'/>
+                        <p>Login</p>
+                    </div>
+                    <div className='flex'>
+                        <CiHeart className='mt-1'/>
+                        <p>Wishlist</p>
+                    </div>
+                    <div className='flex'>
+                        <BsMinecartLoaded className='mt-1'/>
+                        My cart
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
